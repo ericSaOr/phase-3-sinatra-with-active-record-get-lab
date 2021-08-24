ef45abe0e70e3dff3030a5d05ac4ce9705a632bb
Eric Sanchez <ericfsod@gmail.com>
2021-08-24 15:50:20 -0400

    commit tree 9e24a73f4be7c52a26fb571c94e868a371b7422a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,26 @@
-class ApplicationController < Sinatra::Base
+require 'pry'
 
-  # add routes
+class ApplicationController < Sinatra::Base 
+  set :default_content_type, 'application/json'
+  
+  get '/bakeries' do
+    "Hello World"
+    #get all the bakeries from the database
+    bakeries = Bakery.all
+    #send them back as a JSON array
+    bakeries.to_json
+  end
 
+  get '/bakeries/:id' do
+    #get one bakery from database by id
+    bakeries = Bakery.find(params[:id])
+    bakeries.to_json(include:[:baked_goods])
+
+
+   end
+
+   get '/baked_goods/by_price' do
+     baked_goods = BakedGood.all.order(params[:price])
+     baked_goods.to_json(include[:name])
+   end
 end
